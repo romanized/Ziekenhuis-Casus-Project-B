969c569c2c6d9bbc0c41a0d42838436a50ab7335
Ahmad200ali <1116960@hr.nl>
2026-03-26 16:47:56 +0100

    .

--- obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd076061f65ae9217c6ee87b4eff10f2750bb6bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4d9b8dd91f2631a0e1c0e1f901c7f82bc138f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

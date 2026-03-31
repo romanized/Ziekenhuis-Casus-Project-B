689b8538655e8f1039feb54aaa29853ab01d218c
Batman <mohanlaljoshua@gmail.com>
2026-03-31 23:35:23 +0200

    Finished the entire database structure: add hospital appointment database schema with Accounts, Room, Reservation, Specialist, InfoFolder and Notitie tables

--- obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4d9b8dd91f2631a0e1c0e1f901c7f82bc138f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969c569c2c6d9bbc0c41a0d42838436a50ab7335")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

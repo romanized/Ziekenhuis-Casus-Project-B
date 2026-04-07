738c1977504ee3c4b9a3d2a1aa9ed8d57092d114
ShivrajVs <1105109@hr.nl>
2026-04-07 12:34:05 +0200

    Reservation Model made

---
+++ DataModels/ReservationModel.cs
@@ -0,0 +1,37 @@
+public class ReservationModel
+{
+    public Int64 Id { get; set; }
+    public Int64 User_Id { get; set; }
+    public Int64 Room_Id { get; set; }
+    public Int64 Specialist_Id { get; set; }
+    public Int64 Infofolder_Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }
+    public string Status { get; set; }
+    public string Sleutel1 { get; set; }
+    public string Sleutel2 { get; set; }
+
+    public ReservationModel(
+        Int64 id,
+        Int64 userId,
+        Int64 roomId,
+        Int64 specialistId,
+        Int64 infofolderId,
+        DateTime date,
+        string type,
+        string status,
+        string sleutel1,
+        string sleutel2)
+    {
+        Id = id;
+        User_Id = userId;
+        Room_Id = roomId;
+        Specialist_Id = specialistId;
+        Infofolder_Id = infofolderId;
+        Date = date;
+        Type = type;
+        Status = status;
+        Sleutel1 = sleutel1;
+        Sleutel2 = sleutel2;
+    }
+}
--- obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31ca17e94b279010c39d90aefa7c7ab3d734578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f319623a1ce2de455bfea357b3bf635caefa961c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

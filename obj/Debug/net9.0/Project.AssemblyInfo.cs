c72df6d800ab057bda8906535d2ea6f12344bdee
nebi <122598443+romanized@users.noreply.github.com>
2026-04-09 02:00:34 +0200

    Admin kan doctor en planner aanmaken
    - Admin menu met opties voor nieuwe doctor of planner
    - UserLogic.CreateEmployee voor aanmaken met rol en specialty
    - doctor krijgt ook een specialty gevraagd

--- obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a87190911c5ae4faeb015685d2c36aff5d8a996")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ecd2bba2d64f0e8cd0c8311af23df0e9fac8643")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

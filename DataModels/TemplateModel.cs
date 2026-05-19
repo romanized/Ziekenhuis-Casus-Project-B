030f413cf8e7f94f1c9639ffbae90e15d49a4f0c
Joshua <mohanlaljoshua@gmail.com>
2026-05-19 09:46:46 +0100

    voeg TemplateModel toe

---
+++ DataModels/TemplateModel.cs
@@ -0,0 +1,7 @@
+public class TemplateModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Type { get; set; } = "";
+    public string Notes { get; set; } = "";
+}
